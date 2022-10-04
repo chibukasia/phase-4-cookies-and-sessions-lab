d487c525238b9388577c468ea69d0047688aa068
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-10-04 17:07:26 +0300

    commit tree 6dbfde480685e5d2477687d34e41f927aacfc0b5

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,21 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    article = Article.find(params[:id]) 
+    session[:page_views] ||= 0
+    session[:page_views] += 1 
+
+    if session[:page_views] > 3
+      return render json: {error: "Maximum pageview limit reached"}, status: :unauthorized 
+    else
+      return render json: article 
+    end
   end
 
   private
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
-  end
+  end 
 
 end
